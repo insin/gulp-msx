a936d6e22ec3dc00113def7d1e70c579fded5327
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2014-03-21 02:10:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# [gulp]()-msx
+
+(Based on [gulp-react](https://github.com/sindresorhus/gulp-react))
+
+Precompiles [Mithril](http://lhorie.github.io/mithril/) views which use
+[JSX](http://facebook.github.io/react/docs/jsx-in-depth.html) into
+JavaScript, using [msx](https://github.com/insin/msx).
+
+### Install
+
+```
+npm install --save-dev gulp-msx
+```
+
+### Example
+
+```javascript
+var gulp = require('gulp')
+var msx = require('gulp-msx')
+
+gulp.task('transform-jsx', function () {
+  return gulp.src('./src/**/*.jsx')
+    .pipe(msx())
+    .pipe(gulp.dest('./dist'))
+})
+```
+
+The JSX `/** @jsx m */` pragma is prepended to `.jsx` files if missing.
+
+`.jsx` files are automatically renamed to `.js` for you, ready for output.
+
+---
+
+MIT Licensed
---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var path = require('path')
+
+var gutil = require('gulp-util')
+var msx = require('msx')
+var through = require('through2')
+
+module.exports = function(name) {
+  return through.obj(function(file, enc, cb) {
+    if (file.isNull()) {
+      this.push(file)
+      return cb()
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError('gulp-msx', 'Streaming not supported'))
+      return cb()
+    }
+
+    var str = file.contents.toString()
+
+    if (path.extname(file.path) === '.jsx' && str.indexOf('* @jsx') === -1) {
+      str = '/** @jsx m */\n' + str
+    }
+
+    try {
+      file.contents = new Buffer(msx.transform(str))
+      file.path = gutil.replaceExtension(file.path, '.js')
+    }
+    catch (err) {
+      err.fileName = file.path
+      this.emit('error', new gutil.PluginError('gulp-msx', err))
+    }
+
+    this.push(file)
+    cb()
+  })
+}
